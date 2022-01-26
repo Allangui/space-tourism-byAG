6b851877c3b4186cb40bbcabe11a63a856c46674
Allangui <deenv69@hotmail.be>
2022-01-26 10:58:45 +0100

    adding dist

---
+++ dist/assets/navCrew.19b920ba.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.ff75798f.js";import{r as t,o as n,c as a,b as o}from"./vendor.433bd6b6.js";const _={};function c(s,p,d,l,u,v){const e=t("router-link");return n(),a("nav",null,[o(e,{to:"/crew/douglas"}),o(e,{to:"/crew/mark"}),o(e,{to:"/crew/victor"}),o(e,{to:"/crew/anousheh"})])}var m=r(_,[["render",c],["__scopeId","data-v-519278bb"]]);export{m as N};
---
+++ dist/assets/technology.1bbbd396.js
@@ -0,0 +1,1 @@
+import{d as o}from"./data.7b207de7.js";import{_ as t}from"./index.ff75798f.js";import{r,o as a,c as n,b as c}from"./vendor.433bd6b6.js";const s={data(){return{dataTech:o.technology}}};function d(p,_,f,i,l,m){const e=r("router-view");return a(),n("div",null,[c(e,{propTech:this.dataTech},null,8,["propTech"])])}var b=t(s,[["render",d]]);export{b as default};
