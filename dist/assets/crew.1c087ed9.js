9c8ca84686ec1fb180594ce3fcf012720053ebcd
Allangui <deenv69@hotmail.be>
2022-01-26 12:09:59 +0100

    add deploy file

--- dist/assets/crew.1c087ed9.js
+++
@@ -1,1 +0,0 @@
-import{d as r}from"./data.7b207de7.js";import{_ as o}from"./index.ff75798f.js";import{r as t,o as a,c as n,b as s}from"./vendor.433bd6b6.js";const c={data(){return{dataCrew:r.crew}}};function d(p,_,f,i,m,l){const e=t("router-view");return a(),n("div",null,[s(e,{propCrew:this.dataCrew},null,8,["propCrew"])])}var C=o(c,[["render",d]]);export{C as default};
